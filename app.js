3963acf38bb4dbf3f9b3e964b5181e22ba19c371
ChrisLouie10 <christopherlouie16@gmail.com>
2020-12-18 13:33:38 -0800

    Adding Authentication
    Currently, A user can register, log in, and using a auth-token,
    look at posts that require authentication.
    
    Problems:
    -Can't use webpage to register/login. Need to use a rest client.
    -auth-token does not stay with the user (not sure if it will fix itself
    later)

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+//Import Routes
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/post');
 
 mongoose.connect('mongodb://localhost/budget-discord', {
   useUnifiedTopology: true,  
   useNewUrlParser: true
-})
+});
 const db = mongoose.connection
-db.on('error', (error) => console.log(error))
-db.once('open', () => console.log('Connected to Database'))
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Connected to Database'));
 
 app.get('/', (req, res) => {
-  res.send('I eat ass')
+  res.send('I eat ass');
 });
 
-app.listen(3000, () => console.log('connected'))
+app.use(express.json());
+
+app.use('/api/user', authRoute);
+app.use('/api/posts', postRoute);
+
+app.listen(3000, () => console.log('connected'));
