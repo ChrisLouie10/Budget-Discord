73366010f518eb5c089c4f06b1660738ad05c997
ChrisLouie10 <christopherlouie16@gmail.com>
2020-12-19 23:14:55 -0800

    Added a react-based Authorization. This uses firebase to keep track of users. Comments to be added in later commit, depending on if we decide to use firebase as our Authorization system.

--- app.js
+++ app.js
@@ -24,4 +24,4 @@ app.use(express.json());
 app.use('/api/user', authRoute);
 app.use('/api/posts', postRoute);
 
-app.listen(3000, () => console.log('connected'));
+app.listen(5000, () => console.log('connected'));
