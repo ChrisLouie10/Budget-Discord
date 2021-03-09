2994f142f35e19f311a8aec2ab9c5b83869a3a64
Switch24-7 <stephenjusto24@gmail.com>
2021-03-09 01:12:12 -0800

    Installed react-native and react-hook-form. Restyled client Login.js w/ react native components

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,37 +1,28 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
 import { Link, useHistory } from "react-router-dom";
 
 // Simple Login page
 
 export default function Login() {
-
-  const [mounted, setMounted] = useState(true);
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const [error, setError] = useState();
-  const [loading, setLoading] = useState();
   const history = useHistory();
+  const { control, handleSubmit, errors } = useForm();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    return function cleanup(){
-      setMounted(false);
-    }
-  }, []);
-
-  async function handleSubmit(e){
-    e.preventDefault();
-
-    try{
-      setError('');
+  function onSubmit(data){
+    if (!loading){
       setLoading(true);
-      await fetch('/api/user/login', {
+      setError('');
+      fetch('/api/user/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          email: emailRef.current.value,
-          password: passwordRef.current.value
+          email: data.email,
+          password: data.password
         })
       }).then(response => {
         return response.json()})
@@ -39,42 +30,162 @@ export default function Login() {
           if(data.success){
             localStorage.setItem('Authorization', data.Authorization);
             history.push("/dashboard");
-          } else setError(data.message);
+          } else {
+            setError(data.error.login);
+            setLoading(false);
+          }
         })
     }
-    finally{
-      if (mounted) setLoading(false);
-    }
   }
+  
   return (
-    <div className="container d-flex align-items-center justify-content-center"
-        style={{minHeight: "100vh"}}>
-      <div className="w-100" style={{maxWidth: "400px"}}>
-        <div className="card">
-          <div className="card-body">
-            <div className="card-header text-center mb-4">
-             <h2>Log In</h2>
-            </div>
-            {error && <div className="alert alert-danger" role="alert">{error}</div>}
-            <form onSubmit={handleSubmit}>
-              <div className="mb-3" id="email">
-                <label htmlFor="input-email">Email</label>
-                <input type="email" className="form-control" id="input-email" ref={emailRef} required></input>
-              </div>
-              <div className="mb-3" id="password">
-                <label htmlFor="input-password">Password</label>
-                <input type="password" className="form-control" id="input-password" ref={passwordRef} required></input>
-              </div>
-              <button disabled={loading} className="btn btn-primary w-25" type="Submit">Login</button>
-            </form>
-          </div>
-        </div>
-        <div className="w-100 text-center mt-2">
-          Need an account? <Link to="/signup">Sign Up</Link>
-        </div>
-      </div>
-    </div>
-  )
+    <View style={styles.container}>
+      <View style={styles.loginForm}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Log In</Text>
+        </View>
+        <View style={styles.mainContent}>
+          <View style={styles.input}>
+            {errors.email && errors.email.type === 'required' && (
+              <Text style={styles.error}>Please enter your email</Text>
+            )}
+            {errors.email && errors.email.type === 'maxLength' && (
+              <Text style={styles.error}>Email must be 64 characters or less</Text>
+            )}
+            <Controller
+              control={control}
+              render={(props) => (
+                <TextInput
+                  style={styles.textInput}
+                  onBlur={props.onBlur}
+                  onChange={props.onChange}
+                  value={props.value}
+                  ref={props.ref}
+                  placeholder='Email'
+                  textContentType='emailAddress'
+                  autoCapitalize='none'
+                  autoCompleteType='email'
+                  autoCorrect={false}
+                  onSubmitEditing={handleSubmit(onSubmit)}
+                />
+              )}
+              name="email"
+              rules={{ required: true, maxLength: 64 }}
+              defaultValue=""
+            />
+          </View>
+            <View style={styles.input}>
+              {errors.password && errors.password.type === 'required' && (
+                <Text style={styles.error}>Please enter your password</Text>
+              )}
+              {errors.password && errors.password.type === 'minLength' && (
+                <Text style={styles.error}>Password must be at least 10 characters</Text>
+              )}
+              {errors.password && errors.password.type === 'maxLength' && (
+                <Text style={styles.error}>Password must be 32 characters or less</Text>
+              )}
+              <Controller
+                control={control}
+                render={(props) => (
+                  <TextInput
+                    style={styles.textInput}
+                    onBlur={props.onBlur}
+                    onChange={props.onChange}
+                    value={props.value}
+                    ref={props.ref}
+                    placeholder='Password'
+                    textContentType='password'
+                    autoCapitalize='none'
+                    autoCompleteType='password'
+                    autoCorrect={false}
+                    secureTextEntry={true}
+                    onSubmitEditing={handleSubmit(onSubmit)}
+                  />
+                )}
+                name="password"
+                rules={{ required: true, minLength: 10, maxLength: 32 }}
+                defaultValue=""
+              />
+          </View>
+        </View>
+        <View style={styles.bottomSection}>
+          <TouchableOpacity  style={styles.button} onPress={handleSubmit(onSubmit)} type='submit'>
+              <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <Text style={styles.signup}>Need an account? <Link to="/signup">Sign Up</Link></Text>
+    </View>
+  );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100vh'
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#009dff',
+    borderColor: '#009dff',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 5,
+    justifyContent: 'center',
+    marginLeft: 39,
+    height: 45,
+    width: 90
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#fff'
+  },
+  error: {
+    color: '#ff0000',
+    alignSelf: 'flex-start',
+    paddingLeft: '10%'
+  },
+  loginForm: {
+    width: 400,
+    height: 325,
+    borderColor: '#dbdbdb',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 5
+  },
+  header: {
+    flex: .25,
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 30
+  },
+  mainContent: {
+    flex: .5,
+    alignItems: 'center',
+  },
+  bottomSection: {
+    flex: .25,
+    justifyContent: 'center'
+  },
+  input: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  signup: {
+    paddingTop: 10,
+    fontSize: 15
+  },
+  textInput: {
+    height: 50,
+    width: '80%',
+    borderColor: '#dbdbdb',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 5,
+    paddingHorizontal: 10
+  }
+});
--- server/auth/validation.js
+++ server/auth/validation.js
@@ -10,11 +10,25 @@ const registerValidation = (data) => {
 };
 
 const loginValidation = (data) => {
-    const schema = Joi.object({
-        email: Joi.string().max(64).required().email(),
+    const emailSchema = Joi.object({
+        email: Joi.string().max(64).required().email()
+    });
+    const passwordSchema = Joi.object({
         password: Joi.string().max(32).min(10).required()
     });
-    return schema.validate(data);
+    // Validate email and password separately so that we can return
+    // error details for both email and password
+    const emailResult = emailSchema.validate({email: data.email});
+    const passwordResult = passwordSchema.validate({password: data.password});
+
+    if (emailResult.error || passwordResult.error){
+        return {
+            error: {
+                email: emailResult.error ? emailResult.error.details[0].message.replace(/"/g, '') : undefined,
+                password: passwordResult.error ? passwordResult.error.details[0].message.replace(/"/g, '') : undefined
+            }
+        };
+    } else return {};
 };
 
 const updatePasswordValidation = (data) => {
