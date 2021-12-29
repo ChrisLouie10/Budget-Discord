d2724470225d6b4a026cb7293f3b8060fbd308fd
Switch24-7 <stephenjusto24@gmail.com>
2021-12-29 13:23:11 -0800

    refactor auth files to follow eslint rules

--- server/auth/verifyToken.js
+++ server/auth/verifyToken.js
@@ -5,36 +5,31 @@ const User = require('../models/User');
 // returns the user's data if true
 // returns an error if false
 
-const verify = async function (req, res, next){
+module.exports = async function verify(req, res, next) {
+  // check if user has an access token
+  let token = req.header('Authorization');
+  if (token.startsWith('Bearer ')) token = token.slice(7, token.length);
 
-    // check if user has an access token
-    let token = req.header('Authorization');
-    if(token.startsWith('Bearer ')) token = token.slice(7, token.length);
-
-    // verify access token
-    if(token){
-        await jwt.verify(token, process.env.SECRET_AUTH_TOKEN, (err, id) => {
-            if(err) {
-                return res.status(401).json({
-                    success: false, 
-                    message: 'Token is not valid'
-                });
-            } else{
-                User.findOne({_id: id._id}, (err, obj) => {
-                    req.user = obj;
-                    next();
-                });
-            }
+  // verify access token
+  if (token) {
+    // eslint-disable-next-line consistent-return
+    await jwt.verify(token, process.env.SECRET_AUTH_TOKEN, (err, id) => {
+      if (err) {
+        return res.status(401).json({
+          success: false,
+          message: 'Token is not valid',
         });
-    }else{
-        res.status(401).json({
-            success: false,
-            message: 'Auth token is not supplied'
-        });
-    }
-}
-
-module.exports = verify;
-
-
-
+      }
+      // eslint-disable-next-line no-shadow
+      User.findOne({ _id: id._id }, (err, obj) => {
+        req.user = obj;
+        next();
+      });
+    });
+  } else {
+    res.status(401).json({
+      success: false,
+      message: 'Auth token is not supplied',
+    });
+  }
+};
