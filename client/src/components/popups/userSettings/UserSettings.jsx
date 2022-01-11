21905138e2d5b07b92712fa8d8cf31de15d1c879
AnthonyJHizon <AnthonyJHizon714@gmail.com>
2022-01-10 16:32:09 -0800

    Comment out UserSettings file

--- client/src/components/popups/userSettings/UserSettings.jsx
+++ client/src/components/popups/userSettings/UserSettings.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
+// import React from 'react';
 // import PropTypes from 'prop-types';
 // import { Dialog, DialogContent } from '@material-ui/core';
 
-export default function UserSettings({ openPopup, setOpenPopup }) {
-  // function onEscapeKeyDown() {
-  //   setOpenPopup(false);
-  // }
+// export default function UserSettings({ openPopup, setOpenPopup }) {
+//   function onEscapeKeyDown() {
+//     setOpenPopup(false);
+//   }
 
-  return (
-    <>
-    </>
-    // <Dialog>
-    //   <DialogContent fullScreen open={openPopup} onEscapeKeyDown={onEscapeKeyDown} />
-    // </Dialog>
-  );
-}
+//   return (
+//     <Dialog>
+//       <DialogContent fullScreen open={openPopup} onEscapeKeyDown={onEscapeKeyDown} />
+//     </Dialog>
+//   );
+// }
 
 // UserSettings.propTypes = {
 //   openPopup: PropTypes.bool.isRequired,
